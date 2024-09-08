04c96592a834e1582795f06514cc70e10eed0e70
naimulislammunna <naimulislammunna742@gmail.com>
2024-09-09 03:13:51 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port || 4000;
+const port = process.env.port || 7000;
 
 const app = express();
 app.use(cors());
@@ -8,4 +8,8 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('client server ok')
+})
+
+app.listen(port, () => {
+    console.log('port is running:', port);
 })
